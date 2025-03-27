71e0da4872184f87478a2608d4570cbc6be87654
ferramentasia <rdrgslsk@gmail.com>
2025-03-27 13:29:15 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,8 @@ MENSAGENS = {
     ),
     "menu": (
         "📚 *Nossos PDFs Disponíveis:*\n"
-        "Cada PDF custa R$ 10,00 e foi elaborado com muita pesquisa e carinho 📖💛💡📚 Ao clicar aqui vc será direcionado(a) para a página de pagamento do Mercadopago. Selecione o que deseja adquirir, Obrigado 🙏:"
+        "Cada PDF custa R$ 10,00 e foi elaborado com muita pesquisa e carinho 📖💛💡📚 Ao clicar aqui vc será direcionado(a) para a página de pagamento do Mercadopago\.\n"
+        "Selecione o que deseja adquirir\, Obrigado 🙏:"
     ),
     "instrucoes_pagamento": (
         "✅ *Pagamento via PIX*\n\n"
